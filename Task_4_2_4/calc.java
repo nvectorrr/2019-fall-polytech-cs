fa360d9b653ed143ff8ac895833ee2000deb8b10
nvectorrr <55031908+nvectorrr@users.noreply.github.com>
2019-10-24 13:31:21 +0300

    replased by opti

--- Task_4_2_4/calc.java
+++ Task_4_2_4/calc.java
@@ -1,140 +1,112 @@
 import java.util.LinkedList;
 
-class calc
+class calc_opti
 {
-    public static LinkedList<Integer> summ (LinkedList<Integer> num_1, LinkedList<Integer> num_2)
+    public static LinkedList<Integer> calculator (LinkedList<Integer> num_1, LinkedList<Integer> num_2)
     {
         LinkedList<Integer> lst = new LinkedList<Integer>();
-        Integer x, y, s, s1 = 0, s2 = 0;
-        int flag = 0;
+        LinkedList<Integer> lst_h = new LinkedList<Integer>();
+        int flag = 0, x;
 
         if (num_1.getFirst() + num_2.getFirst() >= 10)
         {
             flag = 1;
-        } 
+        }
 
         if (num_1.size() == num_2.size())
         {
-            int i = num_1.size();
-            s1 = 0;
-            while (i > 0)
-            {
-                x = num_1.removeLast();
-                y = num_2.removeLast();
-                s = x + y + s1;
-                if (s >= 10)
-                {
-                    s = s - 10;
-                    s1 = 1;
-                } else
-                {
-                    s1 = 0;
-                }
-                lst.push(s);
-                i--;
-            }
+            lst = summator(num_1, num_2);
             if (flag == 1)
-            {
-                lst.push(1);
-            }     
+            lst.push(1);
         }
+
+        if (num_1.size() > num_2.size())
+        {
+            lst = summator(num_1, num_2);
+            lst_h = extra_summator(num_1, num_2, flag);
+            for (int i = lst_h.size(); i > 0; i--)
+            {
+                x = lst_h.getLast();
+                lst.push(x);
+            }
+        }
+
         else
         {
-            if (num_1.size() > num_2.size())
+            lst = summator(num_2, num_1);
+            lst_h = extra_summator(num_2, num_1, flag);
+            for (int i = lst_h.size(); i > 0; i--)
             {
-                int i = num_2.size();
-                while (i > 0)
-                {
-                    x = num_1.removeLast();
-                    y = num_2.removeLast();
-                    s = x + y + s1;
-                    if (s >= 10)
-                    {
-                        s = s - 10;
-                        s1 = 1;
-                    }
-                    else
-                    {
-                        s1 = 0;
-                    }
-                    lst.push(s);
-                    i--;
-                }
-                
-                i = num_1.size();
-                while (i > 0)
-                {
-                    i--;
-                    x = num_1.removeLast();
-                    if (s2 == 0)
-                    {
-                        if (x == 9 && s1 == 1)
-                        {
-                            lst.push(0);
-                            s2 = 1;
-                        } else
-                        {
-                            lst.push(x);
-                            s1 = 0;
-                            s2 = 0;
-                        }
-                    } else
-                    {
-                        lst.push(x + 1);
-                        s2 = 0;
-                    }
-                    
-                }
-            } else 
-            {
-                int i = num_1.size();
-                while (i > 0)
-                {
-                    x = num_1.removeLast();
-                    y = num_2.removeLast();
-                    s = x + y + s1;
-                    if (s >= 10)
-                    {
-                        s = s - 10;
-                        s1 = 1;
-                    }
-                    else
-                    {
-                        s1 = 0;
-                    }
-                    lst.push(s);
-                    i--;
-                }
-                
-                i = num_2.size();
-                while (i > 0)
-                {
-                    i--;
-                    x = num_2.removeLast();
-                    if (s2 == 0)
-                    {
-                        if (x == 9 && s1 == 1)
-                        {
-                            lst.push(0);
-                            s2 = 1;
-                        } else
-                        {
-                            lst.push(x);
-                            s1 = 0;
-                            s2 = 0;
-                        }
-                    } else
-                    {
-                        lst.push(x + 1);
-                        s2 = 0;
-                    }
-                    
-                }
+                x = lst_h.getLast();
+                lst.push(x);
             }
-                        
         }
-        
 
         return lst;
+    }
+
+    public static LinkedList<Integer> summator (LinkedList<Integer> num_1, LinkedList<Integer> num_2)
+    {
+        LinkedList<Integer> lst1 = new LinkedList<Integer>();
+        
+        int i = num_2.size();
+        while (i > 0)
+        {
+            globals.x = num_1.removeLast();
+            globals.y = num_2.removeLast();
+            globals.s = globals.x + globals.y + globals.s1;
+            
+            if (globals.s >= 10)
+            {
+                globals.s = globals.s - 10;
+                globals.s1 = 1;
+            } 
+            else
+            {
+                globals.s1 = 0;
+            }
+            
+            lst1.push(globals.s);
+            i--;
+        }
+            
+        return lst1;
+    }
+
+    public static LinkedList<Integer> extra_summator (LinkedList<Integer> num_1, LinkedList<Integer> num_2, int flag)
+    {
+        LinkedList<Integer> lst2 = new LinkedList<Integer>();
+        int i = num_1.size();
+
+        if (flag == 1)
+        {
+            globals.s2 = 1;
+        }
+        
+        while (i > 0)
+        {
+            i--;
+            globals.x = num_1.removeLast();
+            if (globals.s2 == 0)
+            {
+                if (globals.x == 9 && globals.s1 == 1)
+                {
+                    lst2.push(0);
+                    globals.s2 = 1;
+                } else
+                {
+                    lst2.push(globals.x);
+                    globals.s1 = 0;
+                    globals.s2 = 0;
+                }
+            } else
+            {
+                lst2.push(globals.x + 1);
+                globals.s2 = 0;
+            }
+                    
+        }
+
+        return lst2;
     }
 }
