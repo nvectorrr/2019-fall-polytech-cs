dab85199d3267837effdf9f0489fec3729cdea24
nvectorrr <55031908+nvectorrr@users.noreply.github.com>
2019-11-02 12:23:37 +0300

    Task 4.1.1(java) added

---
+++ Task_4_1_1_java/pal_check.java
@@ -0,0 +1,22 @@
+class pal_check
+{
+    public static boolean isPalindrome( String s ) 
+    {
+        int n = s.length();
+
+        if( n == 0 )
+        {
+            return false;
+        }
+
+        for( int i = 0; i < (n/2); ++i ) 
+        {
+            if( s.charAt(i) != s.charAt( n - i - 1 ) ) 
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
